3eee984fcc6bc78695ce87dad7ac99bb81fabe34
Ganesh Annan <gannan08@vt.edu>
2017-10-20 17:25:54 -0400

    Make card container more "dumb"
    * Remove max display
    * Remove maintaining two copies of cards withing the container

--- card-container-component.js
+++ card-container-component.js
@@ -8,8 +8,7 @@
 
 export default {
   bindings: {
-    cards: '=brCards',
-    maxDisplay: '<brMaxDisplay'
+    cards: '=brCards'
   },
   transclude: true,
   controller: Ctrl,
@@ -22,48 +21,38 @@ function Ctrl($scope) {
   const self = this;
 
   self.loading = true;
-  let originalCards;
 
   self.$onInit = () => {
-    originalCards = self.cards;
     $scope.$on('MOVE_CARDS_UP', () => self.up());
     $scope.$on('MOVE_CARDS_DOWN', () => self.down());
     $scope.$on('SELECT_IDX', (e, args) => self.select(args.idx));
-    updateCards();
     self.loading = false;
   };
 
   function rightRotate() {
-    const lastCard = originalCards.pop();
-    originalCards = [lastCard, ...originalCards];
+    const lastCard = self.cards.pop();
+    self.cards = [lastCard, ...self.cards];
   }
 
   function leftRotate() {
-    const firstCard = originalCards.shift();
-    originalCards = [...originalCards, firstCard];
+    const firstCard = self.cards.shift();
+    self.cards = [...self.cards, firstCard];
   }
 
   function moveToFront(idx) {
-    const front = originalCards.splice(idx, originalCards.length);
-    originalCards = [...front, ...originalCards];
-  }
-
-  function updateCards() {
-    self.cards = originalCards.slice(0, self.maxDisplay);
+    const front = self.cards.splice(idx, self.cards.length);
+    self.cards = [...front, ...self.cards];
   }
 
   self.select = idx => {
     moveToFront(idx);
-    updateCards();
   }
 
   self.down = () => {
     leftRotate();
-    updateCards();
   }
 
   self.up = () => {
     rightRotate();
-    updateCards();
   }
 }
